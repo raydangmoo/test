10bb50d7a131cb600f26d548c22e2b827e3448cf
RAYMEDICAL\junyoung.hong <raydangmoo@gmail.com>
2023-11-27 13:51:48 +0900

    테스트 파일 생성
    깃과 깃허브 공부용 테스트 파일 작성
    첫 깃 커밋

---
+++ hello.js
@@ -0,0 +1,144 @@
+import React, { useReducer, useRef } from 'react';
+import './App.css';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import New from './pages/New';
+import Edit from './pages/Edit';
+import Diary from './pages/Diary';
+
+const reducer = (state, action) => {
+    let newState = [];
+    switch (action.type) {
+        case 'INIT': {
+            return action.data;
+        }
+        case 'CREATE': {
+            newState = [action.data, ...state];
+            break;
+        }
+        case 'REMOVE': {
+            newState = state.filter((it) => it.id !== action.targetId);
+            break;
+        }
+        case 'EDIT': {
+            newState = state.map((it) =>
+                it.id === action.data.id ? { ...action.data } : it
+            );
+            break;
+        }
+
+        default:
+            return state;
+    }
+    return newState;
+};
+
+export const DiaryStateContext = React.createContext();
+export const DiaryDispatchContext = React.createContext();
+
+const dummyData = [
+    {
+        id: 1,
+        emotion: 1,
+        content: '오늘의 일기 1번',
+        date: 1700722261180,
+    },
+    {
+        id: 2,
+        emotion: 2,
+        content: '오늘의 일기 2번',
+        date: 1700722261181,
+    },
+    {
+        id: 3,
+        emotion: 3,
+        content: '오늘의 일기 3번',
+        date: 1700722261182,
+    },
+    {
+        id: 4,
+        emotion: 1,
+        content: '오늘의 일기 4번',
+        date: 1700722261183,
+    },
+    {
+        id: 5,
+        emotion: 2,
+        content: '오늘의 일기 5번',
+        date: 1700722261184,
+    },
+];
+
+function App() {
+    const [data, dispatch] = useReducer(reducer, dummyData);
+
+    console.log(new Date().getTime());
+    const dataId = useRef(0);
+    // create
+    const onCreate = (date, content, emotion) => {
+        dispatch({
+            type: 'CREATE',
+            data: {
+                id: dataId.current,
+                date: new Date(date).getTime(),
+                content,
+                emotion,
+            },
+        });
+        dataId.current += 1;
+    };
+    // remove
+
+    const onRemove = (targetId) => {
+        dispatch({ type: 'REMOVE', targetId });
+    };
+
+    // edit
+    const onEdit = (targetId, date, content, emotion) => {
+        dispatch({
+            type: 'EDIT',
+            data: {
+                id: targetId,
+                date: new Date(date).getTime(),
+                content,
+                emotion,
+            },
+        });
+    };
+
+    return (
+        <DiaryStateContext.Provider value={data}>
+            <DiaryDispatchContext.Provider
+                value={{
+                    onCreate,
+                    onEdit,
+                    onRemove,
+                }}
+            >
+                <BrowserRouter>
+                    <div className='App'>
+                        <Routes>
+                            <Route
+                                path='/'
+                                element={<Home />}
+                            />
+                            <Route
+                                path='/new'
+                                element={<New />}
+                            />
+                            <Route
+                                path='/edit'
+                                element={<Edit />}
+                            />
+                            <Route
+                                path='/diary/:id'
+                                element={<Diary />}
+                            />
+                        </Routes>
+                    </div>
+                </BrowserRouter>
+            </DiaryDispatchContext.Provider>
+        </DiaryStateContext.Provider>
+    );
+}
+export default App;
